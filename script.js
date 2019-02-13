aeaf71de26bdb181b85ba18a2dac07f26471ee98
kaxkata <gabrielr387@gmail.com>
2019-02-12 22:07:32 -0200

    exercicio

---
+++ script.js
@@ -0,0 +1,16 @@
+let filmes = [{nome: "ratatouille", classificação: 14},
+            {nome: "Harry Potter e a Pedra Filosofal", classificação:14},
+            {nome: "Jogos Vorazes", classificação:16},
+            {nome: "300", classificação:18},
+            {nome:"Mad Max", classificação:18}];
+
+
+let idade;
+
+idade = ()
+
+console.log(idade);
+  if(idade <14){
+      alert(`Você não pode assistir nenhum dos filmes disponiveis, vou chamar seu pais. Vaza! ${}`)
+  }
+
